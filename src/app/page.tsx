69d6abf64700e143556c3d20b4db83c3083b1fee
Ahmad Zaffar <imahmad336@gmail.com>
2025-12-03 19:33:04 +0000

    Header UI Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
 import News from "@/components/News";
 
 export default function Home() {
-    return (
-        <div className="sm:p-12 p-6 sm:w-2xl m-auto">
-            <header className="mb-8">
-                <h4 className="text-xl text-stone-600 underline italic">
-                    News.com
-                </h4>
-            </header>
-            <News />
-        </div>
-    );
+    return <div className="sm:p-12 p-6 sm:w-2xl m-auto"></div>;
 }
